61c3a105bfc3d65325c4421d4685f08e56dd5def
isaacfallon <isaac.fallon@gmail.com>
2024-07-22 18:03:02 +0930

    Setting up initial file structure

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Hi, I&apos;m Isaac Fallon</h1>
+      <h1>Full-Stack Web Developer</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+        non risus mi.
+      </p>
+    </div>
+  );
+}
