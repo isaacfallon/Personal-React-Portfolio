94b0657e1f6fffa514e25f935668551286913d34
isaacfallon <isaac.fallon@gmail.com>
2024-07-25 07:57:39 +0930

    First pass update to overall design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import Nav from './components/Nav';
 
 import './App.css';
 
@@ -8,10 +8,29 @@ function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+     <main>
       <Nav />
-      <main>
+     
         <Outlet />
       </main>
+      <footer className="footer">
+        <ul className="social-list">
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://github.com/isaacfallon">
+                    <i className="fa-brands fa-github"></i>
+                </a>
+            </li>
+            <li className="social-list__item">
+                <a className="social-list__link" href="https://www.linkedin.com/in/isaacfallon/">
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+            </li>
+           
+        </ul>
+        <div className="footer-text">
+            <p className="social-list__item ">&copy; Isaac Fallon 2024 | &lt;Made with React&gt;</p>
+            </div>
+    </footer>
     </>
   );
 }
